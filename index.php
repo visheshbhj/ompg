5abb1ac8ac2f53dd06b6a1bb6fe7ad7438ae7939
Vishesh Bhardwaj <vishesh.bhardwaj.94@gmail.com>
2018-03-11 15:50:25 +0530

    Created Counter

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php include_once("home.html"); ?>
+<?php 
+session_start();
+$fout = fopen("counter.txt", 'r');
+if(!$fout){ 
+    echo"Error 404 File Not Found" ;
+            } else {
+    $counter = (int)fread($fout,20);
+    $counter++;
+    $_SESSION['counter']=$counter;
+    $fin = fopen("counter.txt", "w" );
+    fwrite($fin,$counter);
+    fclose($fout);
+    fclose($fin);
+    header("Location:home.html");
+    }
+?>
---
+++ status.php
@@ -0,0 +1,4 @@
+<?php
+    session_start();
+    echo"<strong> Visitor Counter ".$_SESSION['counter']." </strong> " ;
+?>
